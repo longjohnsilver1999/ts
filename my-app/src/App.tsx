56c8041a9b1d2a9f968bb76875d899e541b50643
sumit katyal <skatyal1999@gmail.com>
2024-01-17 05:28:44 +0000

    setup

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import QuestionCard from './components/QuestionCard';
 import './App.css';
 
 function App() {
@@ -15,6 +15,11 @@ function App() {
   return (
     <div className="App">
     <h1>KBC</h1>
+    <button className='start' onClick={shuruKiaJay}>Kaun banega crorepati.Lets Play</button>
+    <p className='score'>Score:</p>
+    <p>Heres the next question..</p>
+    <QuestionCard />
+    <p></p>
     </div>
   );
 }
