46b256529736143465fee5ea8c50575b333119d6
sumit katyal <skatyal1999@gmail.com>
2024-01-18 04:11:07 +0000

    initialise stae with error

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 import QuestionCard from './components/QuestionCard';
 import './App.css';
 
 function App() {
+  const [loading,setloading]=useState(false)
+  const [questions,setquestions]=useState([])
+  const [number,setnumber]=useState(0)
+  const [userAnswers,setuseranswers]=useState([])
+  const [score,setscore]=useState(0)
+  const [gameover,setgameover]=useState(false)
   const shuruKiaJay=async ()=>{
 
   }
@@ -18,7 +24,7 @@ function App() {
     <button className='start' onClick={shuruKiaJay}>Kaun banega crorepati.Lets Play</button>
     <p className='score'>Score:</p>
     <p>Heres the next question..</p>
-    <QuestionCard />
+    <QuestionCard {}/>
     <button className='next' onClick={aglaSawal}>Next question</button>
     <p></p>
     </div>
