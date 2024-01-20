e77da3ff93e32ae60f1bf3e53803b3fbd1edecd4
sumit katyal <skatyal1999@gmail.com>
2024-01-20 10:01:37 +0530

    important lesson on ts

--- my-app/src/API.ts
+++ my-app/src/API.ts
@@ -3,9 +3,20 @@ EASY="easy",
 MEDIUM="medium",
 HARD="hard"
 }
+//question type dangerouslysethtml
+export type Question={
+category:string;
+correct_answer:string;
+difficulty:string;
+incorrect_answer:string[];
+question:string;
+type:string;
+}
+
+export type QuestionState=Question&{answers:string[]};
 
 export const computerJi=async(amount:number,difficulty:Difficulty)=>{
     const endpont=`https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data=await (await fetch(endpont)).json();
-    console.log(data)
+    return data.results;
 }
