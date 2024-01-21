0cacd138ab23202f736fc39fd564829f58176d9f
sumit katyal <skatyal1999@gmail.com>
2024-01-21 20:10:58 +0530

    check

--- my-app/src/API.ts
+++ my-app/src/API.ts
@@ -9,7 +9,7 @@ export type Question={
 category:string;
 correct_answer:string;
 difficulty:string;
-incorrect_answer:string[];
+incorrect_answers:string[];
 question:string;
 type:string;
 }
@@ -22,8 +22,8 @@ export const computerJi=async(amount:number,difficulty:Difficulty)=>{
     return data.results.map((question:Question)=>(
         {
             ...question,
-            answer:shuffleArray([
-                ...question.incorrect_answer,question.correct_answer
+            answers:shuffleArray([
+                ...question.incorrect_answers,question.correct_answer
             ])
         }
     ));
