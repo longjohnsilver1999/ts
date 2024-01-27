f6a26032762c1478d64a51afe918a00552c4b25a
sumit katyal <skatyal1999@gmail.com>
2024-01-27 11:06:40 +0530

    making arm

--- hangman/src/HangManDrawing.tsx
+++ hangman/src/HangManDrawing.tsx
@@ -12,7 +12,7 @@ const BODY=(
 )
 
 const RIGHT_ARM=(
-    <div style={{width:"100px",height:"10px",background:"black",position:"absolute",top:"150x",right:0 }} />
+    <div style={{width:"100px",height:"10px",background:"black",position:"absolute",top:"150px",right:"-100px",rotate:"-30deg",transformOrigin:"left bottom" }} />
 )
 
 const HangManDrawing=()=>{
