d1f87c73b381d3f724ac1c55a106eb8461e9ed73
sumit katyal <skatyal1999@gmail.com>
2024-01-31 15:03:43 +0530

    making word

--- hangman/src/HangManWord.tsx
+++ hangman/src/HangManWord.tsx
@@ -1,5 +1,9 @@
 const HangManWord=()=>{
-return <div style={{display:"flex",gap:".25em",fontSize:"6rem"}}>
+    const word="test"
+return <div style={{display:"flex",gap:".25em",fontSize:"6rem",fontWeight:"bold",textTransform:"uppercase",fontFamily:"monospace"}}>
+    {word.split("").map((letter,index)=>{
+        <span>{letter}</span>
+    })}
 
 </div>
 }
