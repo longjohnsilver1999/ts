50a1d2a341f7057c2225df407899d4de11db6529
sumit katyal <skatyal1999@gmail.com>
2024-02-02 09:43:22 +0530

    keyboard

--- hangman/src/Keyboard.tsx
+++ hangman/src/Keyboard.tsx
@@ -1,4 +1,39 @@
+const KEYS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ]
+
 const Keyboard=()=>{
+return <div style={{display:"grid",gridTemplateColums:"repeat(auto-fit,minmax(75px,1fr)",gap:".5rem"}}>
+    <div>t</div>
+    <div>t</div>
+    <div>t</div>
+    <div>t</div>
+</div>
 
 }
 export default Keyboard
